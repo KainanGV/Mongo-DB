32380b74af2a6022c54551a89b39510b6037910b
KainanGV <kainangabriel2019@gmail.com>
2022-07-10 00:03:16 -0300

    add mutations

--- src/controllers/TweetController.ts
+++ src/controllers/TweetController.ts
@@ -12,12 +12,57 @@ class TweetController {
         return tweets
     }
 
+    @Query(returns => Tweet, { name: 'tweet' })
+    async findById(
+        @Arg("id") id: string
+    ) {
+        const tweet = await MongoTweet.findById(id)
+
+        if(!tweet) throw new Error("Tweet does not exists")
+
+        return tweet
+    }
+
     @Mutation(returns => Tweet, { name: 'createTweet' })
     async create(
         @Arg("author") author: string,
         @Arg("description") description: string
     ) {
         const tweet = await MongoTweet.create({ author, description, likes: 0 })
+
+        return tweet
+    }
+
+    @Mutation(returns => Tweet)
+    async upvoteTweet(
+        @Arg("id") id: string
+    ) {
+        const tweet = await MongoTweet.findById(id)
+
+        if(!tweet) {
+            throw new Error("Tweet does not exists")
+        }
+
+        tweet.set({ likes: tweet?.likes + 1 })
+
+        await tweet.save()
+
+        return tweet
+    }
+
+    @Mutation(returns => Tweet)
+    async downvoteTweet(
+        @Arg("id") id: string
+    ) {
+        const tweet = await MongoTweet.findById(id)
+
+        if(!tweet) {
+            throw new Error("Tweet does not exists")
+        }
+
+        tweet.set({ likes: tweet?.likes - 1 })
+
+        await tweet.save()
 
         return tweet
     }
